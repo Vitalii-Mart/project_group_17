81534e654bcb5377c00502cefee968db43c66bd8
Vitalii-Mart <110160794+Vitalii-Mart@users.noreply.github.com>
2022-10-18 19:02:00 +0000

    Deploying to gh-pages from @ Vitalii-Mart/project_group_17@63abb183faf791f03cff10b011f1ff61b14a75d6 🚀

---
+++ modal.54ac91fd.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open-buy-n]"),closeModalBtn:document.querySelector("[data-modal-close-buy-n]"),modal:document.querySelector("[data-modal-buy-n]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=modal.54ac91fd.js.map
