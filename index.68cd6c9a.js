f98b7c96e421f2a80ddbefaa3e2e0ba046e7f74b
Vitalii-Mart <110160794+Vitalii-Mart@users.noreply.github.com>
2022-10-20 09:13:19 +0000

    Deploying to gh-pages from @ Vitalii-Mart/project_group_17@a2d7e2cdbd44a655477b06a1c5f803208406bcd8 🚀

---
+++ index.68cd6c9a.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$("a").on("click",(function(o){if(""!==this.hash){o.preventDefault();var t=this.hash;$("html, body").animate({scrollTop:$(t).offset().top},700,(function(){window.location.hash=t}))}}))}));
+//# sourceMappingURL=index.68cd6c9a.js.map
